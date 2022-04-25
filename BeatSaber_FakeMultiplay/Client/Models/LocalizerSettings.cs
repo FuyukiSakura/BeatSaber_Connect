c4cc658aea9a2ac11171a9818e860ea2c3ac1d71
Fuyuki Sakura <fuyuki@sakura.live>
2022-04-25 13:13:06 +1000

    Add basic usage instructions (#5)
    * add credits to all alpha testers
    
    * add localization to nav menu and credit page
    
    * add localization to contact me texts
    
    * Add localisation for Lobby texts
    
    * move the language selector to the top bar
    
    * Add how to instructions
    
    * add installation instructions
    
    * localize the update available text
    
    * add artist category credit

---
+++ BeatSaber_FakeMultiplay/Client/Models/LocalizerSettings.cs
@@ -0,0 +1,25 @@
+﻿namespace BeatSaber_FakeMultiplay.Client.Models
+{
+    public static class LocalizerSettings
+    {
+        /// <summary>
+        /// Gets the default culture of the app
+        /// </summary>
+        public const string NeutralCulture = "en-AU";
+
+        /// <summary>
+        /// Gets a list of cultures supported by the app
+        /// </summary>
+        public static readonly string[] SupportedCultures = { NeutralCulture, "ja-JP", "zh-TW" };
+
+        /// <summary>
+        /// Custom translations of cultures
+        /// </summary>
+        public static readonly (string, string)[] SupportedCulturesWithName =
+        {
+            ("English", NeutralCulture),
+            ("日本語", "ja-JP"),
+            ("繁體中文", "zh-TW")
+        };
+    }
+}
--- BeatSaber_FakeMultiplay/Client/Program.cs
+++ BeatSaber_FakeMultiplay/Client/Program.cs
@@ -1,6 +1,9 @@
 using BeatSaber_FakeMultiplay.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Globalization;
+using BeatSaber_FakeMultiplay.Client.Models;
+using Microsoft.JSInterop;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,4 +11,25 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+builder.Services.AddLocalization();
+
+var host = builder.Build();
+
+CultureInfo culture;
+var js = host.Services.GetRequiredService<IJSRuntime>();
+var result = await js.InvokeAsync<string>("blazorCulture.get");
+
+if (result != null)
+{
+    culture = new CultureInfo(result);
+}
+else
+{
+    culture = new CultureInfo(LocalizerSettings.NeutralCulture);
+    await js.InvokeVoidAsync("blazorCulture.set", LocalizerSettings.NeutralCulture);
+}
+
+CultureInfo.DefaultThreadCurrentCulture = culture;
+CultureInfo.DefaultThreadCurrentUICulture = culture;
+
+await host.RunAsync();
