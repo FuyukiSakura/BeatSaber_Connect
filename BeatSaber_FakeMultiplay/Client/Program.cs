d31674611713ba031fec93b15b6e4fe132215cc0
Fuyuki Sakura <fuyuki@sakura.live>
2024-01-03 19:34:09 +1000

    change setup for local server

--- BeatSaber_FakeMultiplay/Client/Program.cs
+++ BeatSaber_FakeMultiplay/Client/Program.cs
@@ -13,7 +13,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 #if DEBUG
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7218") });
 #else
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://bs-multiplay.azurewebsites.net") });
 #endif
